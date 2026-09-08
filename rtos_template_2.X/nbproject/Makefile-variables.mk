5748f74c73320e827f2f9a79d3ff5941dd745f20
Raymond Lei <pllei@stcloudstate.edu>
2026-09-08 10:33:11 -0500

    initial commit template 2. RB10 is OC3, RB11 is OC2, RB12 is OC1.

---
+++ rtos_template_2.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=rtos_template_2.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/rtos_template_2.X.production.hex
---
+++ rtos_template_2.X/src/app/taskBlinky.c
@@ -0,0 +1,55 @@
+
+#include <stdint.h>
+
+#include "FreeRTOS.h"
+#include "timers.h"
+#include "task.h"
+
+#include <pin_manager.h>
+#include <system.h>
+
+#include "board.h"
+#define taskPRIORITY        1
+#define taskSTACK_SIZE      128
+
+/*****************************************************************************
+
+    Private functions prototype
+
+*****************************************************************************/
+static void vAppTask( void *pvParameters );
+
+/*****************************************************************************
+
+    FreeRTOS Task implementation
+
+*****************************************************************************/
+static inline void vCreatNewTasks(unsigned portBASE_TYPE uxPriority)
+{
+    xTaskCreate(vAppTask, (char const*)"blink", (uint16_t)taskSTACK_SIZE/(uint16_t)(2), NULL, uxPriority, (TaskHandle_t *) NULL );
+}
+
+/*****************************************************************************
+
+    Public functions implementation.
+
+*****************************************************************************/
+void taskBlinky_init(void)
+{
+    vCreatNewTasks(taskPRIORITY);
+}
+
+static void vAppTask( void *pvParameters )
+{
+    /* Just to stop compiler warnings. */
+    (void) pvParameters;
+
+    //===========================================
+    //Task entrance
+    //===========================================
+    while(1)
+    {
+        vTaskDelay(2000);
+//        IO_TOGGLE(LED_1);
+    }
+}
